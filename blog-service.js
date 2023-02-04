ac09f7e050d3525862806a397364616a2b6ac8aa
Jason Shin <jwshin3@myseneca.ca>
2023-02-03 23:04:17 -0500

    assignment 2

---
+++ blog-service.js
@@ -0,0 +1,78 @@
+/*************************************************************************************
+ * WEB322 - Assignment 2
+ * I declare that this assignment is my own work in accordance with the Seneca Academic
+ * Policy. No part of this assignment has been copied manually or electronically from
+ * any other source (including web sites) or distributed to other students.
+ *
+ * Student Name  : Jason Shin
+ * Student ID    : 111569216
+ * Date: Feb 3, 2023
+ * Course/Section: WEB322/NGG
+ * Online (Cyclic) URL: https://blue-different-spider.cyclic.app/
+ *
+ **************************************************************************************/
+const file = require("fs"); // required at the top of your module
+var posts = []; // posts declared as array globally 
+var categories = []; // categories declared as array globally
+
+module.exports.initialize = () => {
+  return new Promise((res, rej) => {
+    file.readFile("./data/posts.json", "utf8", (err, data) => {
+      if (err) {
+        rej(err);
+        console.log("Unable to read file" + err);
+      } else {
+        posts = JSON.parse(data);
+      }
+    });
+
+    file.readFile("./data/categories.json", "utf8", (err, data) => {
+      if (err) {
+        rej(err);
+        console.log("Unable to read file" + err);
+       } else {
+          categories = JSON.parse(data);
+       }
+    });
+    res();
+  });
+};
+
+module.exports.getAllPosts = () => {
+  var allPosts = [];
+  return new Promise((res, rej) => {
+    for (var i = 0; i < posts.length; i++) {
+      allPosts.push(posts[i]);
+    }
+    if (allPosts.length === 0) {
+      rej("no results returned");
+    }
+    res(allPosts);
+  });
+};
+
+module.exports.getPublishedPosts = () => {
+  var publishedPosts = [];
+  return new Promise((res, rej) => {
+    for (var j = 0; j < posts.length; j++) {
+      if (posts[j].published == true) {
+        publishedPosts.push(posts[j]);
+      }
+    }
+
+    if (publishedPosts.length === 0) {
+      rej("no results returned");
+    }
+    res(publishedPosts);
+  });
+};
+
+module.exports.getCategories = () => {
+  return new Promise((res, rej) => {
+    if (categories.length == 0) {
+      rej("no results returned");
+    } else {
+      res(categories);
+    }
+  });
+};
--- server.js
+++ server.js
@@ -1,25 +1,84 @@
 /*************************************************************************************
-* WEB322 - Assignment 1
-* I declare that this assignment is my own work in accordance with the Seneca Academic
-* Policy. No part of this assignment has been copied manually or electronically from
-* any other source (including web sites) or distributed to other students.
-*
-* Student Name  : Jason Shin
-* Student ID    : 111569216
-* Date: January 20, 2023
-* Course/Section: WEB322/NGG
-* Online (Cyclic) URL: https://black-bullfrog-gear.cyclic.app 
-*
-**************************************************************************************/
+ * WEB322 - Assignment 2
+ * I declare that this assignment is my own work in accordance with the Seneca Academic
+ * Policy. No part of this assignment has been copied manually or electronically from
+ * any other source (including web sites) or distributed to other students.
+ *
+ * Student Name  : Jason Shin
+ * Student ID    : 111569216
+ * Date: Feb 3, 2023
+ * Course/Section: WEB322/NGG
+ * Online (Cyclic) URL: https://blue-different-spider.cyclic.app/
+ *
+ **************************************************************************************/
 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+const path = require("path");
+const blogService = require("./blog-service.js");
 
-// setup a 'route' to listen on the default url path
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
 app.get("/", (req, res) => {
-    res.send("Jason Shin - 111569216");
+  res.redirect("/about");
 });
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.get("/blog", (req, res) => {
+  blogService
+    .getPublishedPosts()
+    .then((data) => {
+      console.log("getPublishedPosts.json");
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log("Unable to open the file: " + err);
+      res.json({ message: err });
+    });
+});
+
+app.get("/posts", (req, res) => {
+  blogService
+    .getAllPosts()
+    .then((data) => {
+      console.log("getAllPosts.json");
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log("Unable to open the file: " + err);
+      res.json({ message: err });
+    });
+});
+
+app.get("/categories", (req, res) => {
+  blogService
+    .getCategories()
+    .then((data) => {
+      console.log("getCategories.json");
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log("Unable to open the file: " + err);
+      res.json({ message: err });
+    });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+blogService
+  .initialize()
+  .then(() => {
+    console.log("Initialized!");
+    app.listen(HTTP_PORT, onHttpStart);
+  })
+  .catch((err) => {
+    console.log("Initialize error has occurred:" + err);
+  });
